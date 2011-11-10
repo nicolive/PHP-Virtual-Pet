a33390b9bf008aaeef7b2022f3f4267d7226660d
Nico <nico@nicowilfer.com>
2011-11-10 10:47:08 +0100

    config file added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Get these from http://developers.facebook.com
+$api_key = 'YOUR_FB_API_KEY';
+$secret  = 'YOUR_FB_SECRET';
+
+$DB[server] = 'localhost';
+$DB[user] = 'root';
+$DB[pass] = '';
+$DB[db] = 'tamagotchi';
+
+?>
